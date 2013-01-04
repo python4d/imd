b8d8cc4053cb0638e24833b847cf55ff3a222068
python4d <contact@python4d.com>
2013-01-05 00:14:23 +0100

    Icone/Splash/STL/BITMAP/ctrl float&int/OpenGLVTK/STLexperimental
    Signed-off-by: python4d <contact@python4d.com>

--- IMD3D/Appli_IMD3D.py
+++ IMD3D/Appli_IMD3D.py
@@ -5,6 +5,7 @@ Créé le 28 nov. 2012
 Code minimum pour créer l'application IMD3D et lancer l'interface
 Mode debug ne permet pas de pour bénéficier du mulprocessing (cf MainApp.py et NoteBook.py)
 @author: Python4D/Dynamic3D
+@version: alpha
 '''
 from IMD3D import cIMD3D
 
